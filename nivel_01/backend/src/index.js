74cae0c62f0f32589b67d25f6d7bda6b4a562835
Pedro Moreno <nennogabriel@gmail.com>
2020-07-07 19:06:05 -0300

    01a07 - Tipos de parametros

--- nivel_01/backend/src/index.js
+++ nivel_01/backend/src/index.js
@@ -2,40 +2,60 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json())
+
 app.get("/projects", (request, response) => {
-    return response.json([
-        "Projeto 1",
-        "Projeto 2",
-    ]
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner)
+
+  return response.json([
+    "Projeto 1",
+    "Projeto 2",
+  ]
   )
 })
 
 app.post("/projects", (request, response) => {
-    return response.json([
-        "Projeto 1",
-        "Projeto 2",
-        "Projeto 3",
-    ]
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
+  return response.json([
+    "Projeto 1",
+    "Projeto 2",
+    "Projeto 3",
+  ]
   )
 })
 
 app.put("/projects/:id", (request, response) => {
-    return response.json([
-        "Projeto 4",
-        "Projeto 2",
-        "Projeto 3",
-    ]
+  const { id } = request.params;
+
+  console.log(id);
+
+  return response.json([
+    "Projeto 4",
+    "Projeto 2",
+    "Projeto 3",
+  ]
   )
 })
 
 app.delete("/projects/:id", (request, response) => {
-    return response.json([
-        "Projeto 2",
-        "Projeto 3",
-    ]
+  const params = request.params;
+
+  console.log(params);
+
+  return response.json([
+    "Projeto 2",
+    "Projeto 3",
+  ]
   )
 })
 
 app.listen(3333, () => {
-    console.log("🚀 Backend started!")
+  console.log("🚀 Backend started!")
 })
