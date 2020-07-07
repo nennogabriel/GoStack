4a0467b52720709e9606bd9080bf6a47a482e7db
Pedro Moreno <nennogabriel@gmail.com>
2020-07-06 21:29:26 -0300

    01a04 - Configurando nodemon

--- nivel_01/backend/src/index.js
+++ nivel_01/backend/src/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-    return response.json({messsage: "Hello world"})
+    return response.json({messsage: "Hello GoStack"})
 })
 
-app.listen(3333)
+app.listen(3333, () => {
+    console.log("🚀 Backend started!")
+})
