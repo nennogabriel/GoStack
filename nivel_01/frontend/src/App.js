c0ad56f6029a45c29ccf89b3cf4dc7ce8417a17f
Pedro Moreno <nennogabriel@gmail.com>
2020-07-08 20:40:29 -0300

    02a06 - Importando CSS e imagens

--- nivel_01/frontend/src/App.js
+++ nivel_01/frontend/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import './App.css'
+
+import backgroundImage from './backgound.jpg'
+
 import Header from './components/Header.js'
 
 function App() {
   return (
     <>
       <Header title="Homepage">
+        <img width={300} src={backgroundImage} alt="" />
         <ul>
           <li>Homepage</li>
           <li>Projects</li>
