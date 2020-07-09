01be0c8c88b69aa02537adc0072a1ff32a1a31cd
Pedro Moreno <nennogabriel@gmail.com>
2020-07-08 21:07:40 -0300

    02a07 - Listando Projetos da api

--- nivel_01/frontend/src/App.js
+++ nivel_01/frontend/src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import api from './services/api.js'
 
 import './App.css'
-
-import backgroundImage from './backgound.jpg'
 
 import Header from './components/Header.js'
 
 function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data)
+    })
+
+  }, [])
+
   return (
     <>
-      <Header title="Homepage">
-        <img width={300} src={backgroundImage} alt="" />
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
       <Header title="Projects">
-        <ul>
-          <li>Homepage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
       </Header>
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
     </>
   );
 }
