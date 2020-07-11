81774e499d2750d1257ca6f76d72469845c8c863
Pedro Moreno <nennogabriel@gmail.com>
2020-07-11 09:43:42 -0300

    04a04 - Tipando objetos e vetores

--- nivel_01/typescript/src/routes.ts
+++ nivel_01/typescript/src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express'
+import createUser from './service/CreateUser'
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'pedro@seal.works',
+    password: '123456',
+    techs: [
+      'NodeJs',
+      'ReactJS',
+      'React Native',
+      { title: 'JavaScript', expirience: 100 }
+    ]
+  })
+
   return response.json({ message: "Hello wolrd" });
 }
