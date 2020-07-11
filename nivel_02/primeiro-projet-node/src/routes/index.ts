a6736319eb97ddbe0bf3b488fceb5b76ef52354c
Pedro Moreno <nennogabriel@gmail.com>
2020-07-11 20:50:42 -0300

    04a06 - Cadastro de Agendamentos

--- nivel_02/primeiro-projet-node/src/routes/index.ts
+++ nivel_02/primeiro-projet-node/src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-  return response.json({ name, email });
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
