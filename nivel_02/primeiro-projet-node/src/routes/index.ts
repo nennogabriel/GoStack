c41b823150d29d041dc1c36ca13b37b1f2f44c8b
Pedro Moreno <nennogabriel@gmail.com>
2020-07-11 19:57:24 -0300

    04a05 - Debugando NodeJS

--- nivel_02/primeiro-projet-node/src/routes/index.ts
+++ nivel_02/primeiro-projet-node/src/routes/index.ts
@@ -1,4 +1,10 @@
 import { Router } from 'express';
 
 const routes = Router();
+
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+  return response.json({ name, email });
+});
+
 export default routes;
--- nivel_02/primeiro-projet-node/src/server.ts
+++ nivel_02/primeiro-projet-node/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import routes from './routes'
-const app = express();
+import cors from 'cors';
+import routes from './routes';
 
-app.get("/", (request, response) => {
-  return response.json({ message: 'Hello World!' });
-})
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-})
+  console.log('🚀 Server started on port 3333!');
+});
