23e3cfd6aba23bdd3aaad6b71c0863077b8990e7
Pedro Moreno <nennogabriel@gmail.com>
2020-07-12 11:41:34 -0300

    fix: eslint with typescript

--- aulas/primeiro_projeto/backend/src/routes/appointments.routes.ts
+++ aulas/primeiro_projeto/backend/src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
@@ -7,10 +8,17 @@ const appointments = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
