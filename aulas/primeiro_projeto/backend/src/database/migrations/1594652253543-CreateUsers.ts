23d0f505bc4985bd2bbd67f33e87fdcd84b0aacf
Pedro Moreno <nennogabriel@gmail.com>
2020-07-14 14:00:28 -0300

    06a06 - Model e migration de usuário

--- aulas/primeiro_projeto/backend/src/database/migrations/1594652253543-CreateUsers.ts
+++ aulas/primeiro_projeto/backend/src/database/migrations/1594652253543-CreateUsers.ts
@@ -1,14 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1594603705399
-  implements MigrationInterface {
+export default class CreateUsers1594652253543 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
-
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -18,12 +15,17 @@ export default class CreateAppointments1594603705399
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -41,6 +43,6 @@ export default class CreateAppointments1594603705399
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
