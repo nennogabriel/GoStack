6b62982c8771f50e8869c148ec30d299ea37aa86
Pedro Moreno <nennogabriel@gmail.com>
2020-07-14 18:48:56 -0300

    06a12 - Rotas autenticadas

---
+++ aulas/primeiro_projeto/backend/src/config/authConfig.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'chavegeradanomd5',
+    expiresIn: '30d',
+  },
+};
--- aulas/primeiro_projeto/backend/src/services/AutenticateUserService.ts
+++ aulas/primeiro_projeto/backend/src/services/AutenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import User from '../models/User';
+import authConfig from '../config/authConfig';
 
 interface Request {
   email: string;
@@ -29,9 +30,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination');
     }
 
-    const token = sign({}, 'chavegeradanomd5', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '30d',
+      expiresIn,
     });
     return { user, token };
   }
