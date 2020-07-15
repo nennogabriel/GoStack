38a83fc22d9b11b553f81ce08d0293dc89fc01a3
Pedro Moreno <nennogabriel@gmail.com>
2020-07-14 23:59:35 -0300

    06a15 - Servindo arquivos estaticos

--- aulas/primeiro_projeto/backend/src/server.ts
+++ aulas/primeiro_projeto/backend/src/server.ts
@@ -4,9 +4,11 @@ import express from 'express';
 import routes from './routes';
 
 import './database';
+import uploadConfig from './config/uploadConfig';
 
 const app = express();
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
