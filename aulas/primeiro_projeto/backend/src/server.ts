e2df77c521bbffb572c96a8a31c9062998733173
Pedro Moreno <nennogabriel@gmail.com>
2020-07-19 12:24:10 -0300

    fix: spaces with eslint

--- aulas/primeiro_projeto/backend/src/server.ts
+++ aulas/primeiro_projeto/backend/src/server.ts
@@ -2,8 +2,8 @@ import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-import routes from './routes';
 
+import routes from './routes';
 import './database';
 import uploadConfig from './config/uploadConfig';
 import AppError from './errors/AppError';
