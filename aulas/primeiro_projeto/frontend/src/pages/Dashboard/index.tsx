179afa62a490a4589a21fa42fe462dbf252f4095
Pedro Moreno <nennogabriel@gmail.com>
2020-07-20 22:08:26 -0300

    07a08 - Navegando Entre Rotas

--- aulas/primeiro_projeto/frontend/src/pages/Dashboard/index.tsx
+++ aulas/primeiro_projeto/frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, FormEvent } from 'react';
-
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
@@ -70,7 +70,10 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -80,7 +83,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
