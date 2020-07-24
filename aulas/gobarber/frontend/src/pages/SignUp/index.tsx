8f7ccf2dbff078c02b3d308ee7cd4cb5f14ee82c
Pedro Moreno <nennogabriel@gmail.com>
2020-07-23 21:13:13 -0300

    08a05 - Pagina de cadastro

---
+++ aulas/gobarber/frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Bakcground } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Bakcground />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+      <form>
+        <h1>Faça seu cadastro</h1>
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="passowrd"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="/">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
