d23ac172c3de69c13b4ef8400706270b41dd864b
Pedro Moreno <nennogabriel@gmail.com>
2020-07-25 09:47:19 -0300

    08a13 - API de Contexto

--- aulas/gobarber/frontend/src/pages/SignIn/index.tsx
+++ aulas/gobarber/frontend/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
@@ -12,9 +12,13 @@ import Button from '../../components/Button';
 
 import { Container, Content, Bakcground } from './styles';
 import getValidationErrors from '../../utils/getValidationErrors';
+import { AuthContext } from '../../context/AuthContext';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
       formRef.current?.setErrors({});
