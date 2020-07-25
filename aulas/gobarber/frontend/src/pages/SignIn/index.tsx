5c7e68ff69ec8087eee6de9d78eba185d180db6b
Pedro Moreno <nennogabriel@gmail.com>
2020-07-25 15:26:26 -0300

    08a16 - criando hook useAuth

--- aulas/gobarber/frontend/src/pages/SignIn/index.tsx
+++ aulas/gobarber/frontend/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
@@ -12,7 +12,7 @@ import Button from '../../components/Button';
 
 import { Container, Content, Bakcground } from './styles';
 import getValidationErrors from '../../utils/getValidationErrors';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 interface SignInFormData {
   email: string;
@@ -22,9 +22,7 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { user, signIn } = useContext(AuthContext);
-
-  console.log(user);
+  const { signIn } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
