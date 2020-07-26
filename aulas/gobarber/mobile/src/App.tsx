38dc98ab375e3442436a752dec8318bb641b9c8b
Pedro Moreno <nennogabriel@gmail.com>
2020-07-26 17:50:50 -0300

    09a03 - Configurando navegação

--- aulas/gobarber/mobile/src/App.tsx
+++ aulas/gobarber/mobile/src/App.tsx
@@ -1,14 +1,27 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
 import { View, StatusBar } from 'react-native';
+
+import Routes from './routes';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-    </>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#312e38',
+        }}
+      >
+        <Routes />
+      </View>
+    </NavigationContainer>
   );
 };
 
