f0cce8b477da22ac06b1a9954aed3508428431c0
Pedro Moreno <nennogabriel@gmail.com>
2020-07-26 16:37:05 -0300

    09a01.1 - Configurando Estrutura

--- aulas/gobarber/mobile/src/App.tsx
+++ aulas/gobarber/mobile/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  return <View />;
+  return <><StatusBar /><View style={{ flex: 1, backgroundColor: '#312e38' }} /></>;
 };
 
 export default App;
