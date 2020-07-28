14b8d61b0b4bd1b3e2de293cb8aa5601094f2c86
Pedro Moreno <nennogabriel@gmail.com>
2020-07-27 21:44:40 -0300

    09a07 - foco e preenchimento

--- aulas/gobarber/mobile/src/pages/SignIn/index.tsx
+++ aulas/gobarber/mobile/src/pages/SignIn/index.tsx
@@ -1,23 +1,106 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 
-import { Image } from 'react-native';
+import {
+  View,
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
+import { useNavigation } from '@react-navigation/native';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+  const inputPasswordRef = useRef<TextInput>(null);
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, []);
+  const formRef = useRef<FormHandles>(null);
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Faça seu Login</Title>
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-      <Button onPress={() => { }}>Entrar</Button>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu Login</Title>
+            </View>
+            <Form
+              style={{ width: '100%' }}
+              onSubmit={handleSignIn}
+              ref={formRef}
+            >
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  inputPasswordRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={inputPasswordRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
+            <ForgotPassword>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton
+        onPress={() => {
+          navigation.navigate('SignUp');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Crie sua conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
--- aulas/gobarber/mobile/src/pages/SignUp/index.tsx
+++ aulas/gobarber/mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,107 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 
-import { Container } from './styles';
+import {
+  View,
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+
+import { useNavigation } from '@react-navigation/native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
-  return <Container />;
+  const formRef = useRef<FormHandles>(null);
+  const inputEmailRef = useRef<TextInput>(null);
+  const inputPasswordRef = useRef<TextInput>(null);
+  const navigation = useNavigation();
+  const handleSubmit = useCallback((data: object) => {
+    console.log(data);
+  }, []);
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+            <Form
+              style={{ width: '100%' }}
+              onSubmit={handleSubmit}
+              ref={formRef}
+            >
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  inputEmailRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={inputEmailRef}
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  inputPasswordRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={inputPasswordRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                textContentType="newPassword"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignIn
+        onPress={() => {
+          navigation.navigate('SignIn');
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar para Logon</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
