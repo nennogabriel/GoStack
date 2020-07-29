fadb7ab1ce445cd0d21d787067e5f0069422765e
Pedro Moreno <nennogabriel@gmail.com>
2020-07-29 11:57:54 -0300

    10a14 - Pensando nos testes

--- aulas/gobarber/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ aulas/gobarber/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,11 @@
-test('should return a sum of two number', () => {
-  expect(1 + 2).toBe(3);
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create an appointment', () => {
+    expect(1).toBe(3);
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1).toBe(3);
+  // });
 });
