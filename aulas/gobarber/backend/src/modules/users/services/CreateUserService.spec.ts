8117846e32c4af92139a6af94bd2cea06563d41f
Pedro Moreno <nennogabriel@gmail.com>
2020-07-29 14:02:21 -0300

    10a18 - Testando criação de usuario

--- aulas/gobarber/backend/src/modules/users/services/CreateUserService.spec.ts
+++ aulas/gobarber/backend/src/modules/users/services/CreateUserService.spec.ts
@@ -2,17 +2,34 @@ import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
-describe('CreateAppointment', () => {
-  it('should be able to create an appointment', async () => {
+describe('CreateUser', () => {
+  it('should be able to create an user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createAppointment = new CreateUserService(fakeUsersRepository);
-    const appointment = await createAppointment.execute({
+    const createUser = new CreateUserService(fakeUsersRepository);
+    const user = await createUser.execute({
       name: 'Pedro Moreno',
       email: 'pedro@example.com',
       password: 'password123',
     });
 
-    expect(appointment).toHaveProperty('id');
-    expect(appointment.email).toEqual('pedro@example.com');
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create an new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+    await createUser.execute({
+      name: 'Pedro Moreno',
+      email: 'pedro@example.com',
+      password: 'password123',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'Pedro Moreno',
+        email: 'pedro@example.com',
+        password: 'password123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
