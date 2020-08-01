1ac3dc2ac981b75eafe687ad3a2349fe798b64cf
Pedro Moreno <nennogabriel@gmail.com>
2020-08-01 15:38:38 -0300

    11a13 - Listando dias Disponiveis

--- aulas/gobarber/backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ aulas/gobarber/backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -14,19 +14,47 @@ describe('ListProviderMonthAvailabilityService', () => {
   it('should be able to list the month availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 3, 20, 8, 0, 0),
+      date: new Date(2020, 4, 8, 8, 0, 0),
     });
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
+      date: new Date(2020, 4, 8, 9, 0, 0),
     });
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
+      date: new Date(2020, 4, 8, 10, 0, 0),
     });
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 21, 8, 0, 0),
+      date: new Date(2020, 4, 8, 11, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 8, 12, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 8, 13, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 8, 14, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 8, 15, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 8, 16, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 8, 17, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 9, 8, 0, 0),
     });
 
     const availability = await listProviderMonthAvailabilityService.execute({
@@ -35,11 +63,13 @@ describe('ListProviderMonthAvailabilityService', () => {
       month: 5,
     });
 
-    expect.arrayContaining([
-      { day: 19, availability: false },
-      { day: 20, availability: true },
-      { day: 21, availability: true },
-      { day: 22, availability: false },
-    ]);
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 8, available: false },
+        { day: 20, available: true },
+        { day: 21, available: true },
+        { day: 22, available: true },
+      ]),
+    );
   });
 });
--- aulas/gobarber/backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ aulas/gobarber/backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 interface IRequest {
   provider_id: string;
@@ -29,9 +30,24 @@ class ListProviderMonthAvailabilityService {
       { provider_id, year, month },
     );
 
-    console.log(appointments);
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
 
-    return [{ day: 20, available: true }];
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(
+        appointment => getDate(appointment.date) === day,
+      );
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
