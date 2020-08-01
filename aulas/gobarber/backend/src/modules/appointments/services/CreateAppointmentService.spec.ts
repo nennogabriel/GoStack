c76936ef3fe089858da61423431ed11d62bcfe00
Pedro Moreno <nennogabriel@gmail.com>
2020-08-01 20:39:48 -0300

    11a17 - Regras do agendamento

--- aulas/gobarber/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ aulas/gobarber/backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -11,31 +11,70 @@ describe('CreateAppointment', () => {
     createAppointment = new CreateAppointmentService(
       fakeAppointmentsRepository,
     );
+    jest
+      .spyOn(Date, 'now')
+      .mockImplementation(() => new Date(2020, 4, 10, 12).getTime());
   });
   it('should be able to create an appointment', async () => {
     const appointment = await createAppointment.execute({
-      provider_id: '123412',
-      user_id: '123412',
-      date: new Date(),
+      provider_id: 'provider-id',
+      user_id: 'user-id',
+      date: new Date(2020, 4, 10, 13),
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toEqual('123412');
+    expect(appointment.provider_id).toEqual('provider-id');
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const appointmentDate = new Date(2020, 4, 10, 11);
     await createAppointment.execute({
-      provider_id: '123412',
-      user_id: '123412',
-      date: appointmentDate,
+      provider_id: 'provider-id',
+      user_id: 'user-id',
+      date: new Date(2020, 4, 10, 13),
     });
 
     await expect(
       createAppointment.execute({
-        provider_id: '123412',
-        user_id: '123412',
-        date: appointmentDate,
+        provider_id: 'provider-id',
+        user_id: 'user2-id',
+        date: new Date(2020, 4, 10, 13),
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment on a past date', async () => {
+    await expect(
+      createAppointment.execute({
+        provider_id: 'provider-id',
+        user_id: 'user-id',
+        date: new Date(2020, 4, 8, 12),
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment with same user as provider', async () => {
+    await expect(
+      createAppointment.execute({
+        provider_id: 'provider-id',
+        user_id: 'provider-id',
+        date: new Date(2020, 4, 10, 13),
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment before 8 a.m. of before 5 p.m.', async () => {
+    await expect(
+      createAppointment.execute({
+        provider_id: 'provider-id',
+        user_id: 'user-id',
+        date: new Date(2020, 4, 10, 7),
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+    await expect(
+      createAppointment.execute({
+        provider_id: 'provider-id',
+        user_id: 'user-id',
+        date: new Date(2020, 4, 10, 18),
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
--- aulas/gobarber/backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ aulas/gobarber/backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import AppError from '@shared/errors/AppError';
@@ -24,6 +24,17 @@ class CreateAppointmentService {
   }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
+    if (isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointment on a past date");
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError("You can't create yourself an appointment'");
+    }
+
+    if (getHours(date) < 8 || getHours(date) > 17) {
+      throw new AppError("You can't create an appointment that hour'");
+    }
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
