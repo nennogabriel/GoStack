5440085f434b9bcac8316cfac205c96a7cb411dd
Pedro Moreno <nennogabriel@gmail.com>
2020-08-01 18:22:18 -0300

    11a15 - excluindo horarios antigos

--- aulas/gobarber/backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ aulas/gobarber/backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -21,6 +21,10 @@ describe('ListProviderDayAvailabilityService', () => {
       date: new Date(2020, 4, 20, 10, 0, 0),
     });
 
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 19, 11).getTime();
+    });
+
     const availability = await listProviderDayAvailabilityService.execute({
       provider_id: 'user',
       year: 2020,
@@ -34,6 +38,40 @@ describe('ListProviderDayAvailabilityService', () => {
         { hour: 9, available: true },
         { hour: 10, available: false },
         { hour: 11, available: true },
+        { hour: 17, available: true },
+      ]),
+    );
+  });
+
+  it('should be able unavaliable hour if time is past', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
+    });
+
+    const availability = await listProviderDayAvailabilityService.execute({
+      provider_id: 'user',
+      year: 2020,
+      month: 5,
+      day: 20,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 12, available: true },
         { hour: 17, available: true },
       ]),
     );
--- aulas/gobarber/backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ aulas/gobarber/backend/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 // import { getDaysInMonth, getDate } from 'date-fns';
 
 interface IRequest {
@@ -37,13 +37,19 @@ class ListProviderDayAvailabilityService {
       { length: 10 },
       (_, index) => index + hourStart,
     );
+
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
+
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
     return availability;
