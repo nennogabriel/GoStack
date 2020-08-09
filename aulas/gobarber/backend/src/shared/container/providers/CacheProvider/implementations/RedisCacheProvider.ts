550c36e83a19f662052adfab190542d8aaccb855
Pedro Moreno <nennogabriel@gmail.com>
2020-08-09 15:28:39 -0300

    11a29 - cache lista de providers

--- aulas/gobarber/backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ aulas/gobarber/backend/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -9,11 +9,15 @@ export default class RedisCacheProvider implements ICacheProvider {
     this.client = new Redis(cacheConfig.config.redis);
   }
 
-  public async save(key: string, value: string): Promise<void> {
-    await this.client.set(key, value);
+  public async save(key: string, value: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(value));
   }
 
-  public async recover(key: string): Promise<string | undefined> { }
+  public async recover<T>(key: string): Promise<T | undefined> {
+    const data = await this.client.get(key);
+    const parsedData = JSON.parse(data || '') as T;
+    return parsedData;
+  }
 
   public async invalidate(key: string): Promise<void> { }
 }
