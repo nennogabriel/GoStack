b799239af8d39ecdc22949231908bbb7f2e8046b
Pedro Moreno <nennogabriel@gmail.com>
2020-08-17 16:11:32 -0300

    12a04 - Dias disponíveis no mês

--- aulas/gobarber/backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ aulas/gobarber/backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 interface IRequest {
   provider_id: string;
@@ -38,12 +38,15 @@ class ListProviderMonthAvailabilityService {
     );
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
+
       const appointmentsInDay = appointments.filter(
         appointment => getDate(appointment.date) === day,
       );
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
