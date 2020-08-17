d4a1fb9a5019ef2cf6004d0b9afff25eb5d8cd65
Pedro Moreno <nennogabriel@gmail.com>
2020-08-17 15:58:45 -0300

    12a02 - utilizando query params

--- aulas/gobarber/backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ aulas/gobarber/backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -5,14 +5,14 @@ import ListProviderMonthAvailabilityService from '@modules/appointments/services
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { year, month } = request.body;
+    const { year, month } = request.query;
     const listProviderMonthAvailabilityService = container.resolve(
       ListProviderMonthAvailabilityService,
     );
     const availability = await listProviderMonthAvailabilityService.execute({
       provider_id,
-      year,
-      month,
+      year: Number(year),
+      month: Number(month),
     });
 
     return response.json(availability);
