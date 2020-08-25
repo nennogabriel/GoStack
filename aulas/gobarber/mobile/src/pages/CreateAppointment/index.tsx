bab66390ae5faf2d000bf67bc6b2f2167c63a27a
Pedro Moreno <nennogabriel@gmail.com>
2020-08-25 18:59:26 -0300

    13a09 - Estrutura da criação

--- aulas/gobarber/mobile/src/pages/CreateAppointment/index.tsx
+++ aulas/gobarber/mobile/src/pages/CreateAppointment/index.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { useRoute, useNavigation } from '@react-navigation/native';
+
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 
 const CreateAppointment: React.FC = () => {
+  const route = useRoute();
+  const { user } = useAuth();
+  const { providerId } = route.params as RouteParams;
+  const { goBack } = useNavigation();
+
   return (
     <Container>
-      <Text>CreateAppointment</Text>
+      <Header>
+        <BackButton
+          onPress={() => {
+            goBack();
+          }}
+        >
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
     </Container>
   );
 };
