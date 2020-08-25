5ba93232aee65e354f1d522ca37645200772e2f6
Pedro Moreno <nennogabriel@gmail.com>
2020-08-25 16:35:50 -0300

    13a04 - Criando páginas e rotas

---
+++ aulas/gobarber/mobile/src/pages/AppointmentCreted/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const AppointmentCreted: React.FC = () => {
+  return (
+    <Container>
+      <Text>AppointmentCreted</Text>
+    </Container>
+  );
+};
+
+export default AppointmentCreted;
