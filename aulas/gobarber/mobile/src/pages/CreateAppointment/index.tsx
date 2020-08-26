8b50a06d6dc1582fde123be0d90fc4448b48acac
Pedro Moreno <nennogabriel@gmail.com>
2020-08-25 21:34:50 -0300

    13a12 -Buscando disponibilidade na API

--- aulas/gobarber/mobile/src/pages/CreateAppointment/index.tsx
+++ aulas/gobarber/mobile/src/pages/CreateAppointment/index.tsx
@@ -36,6 +36,11 @@ export interface Provider {
   avatar_url: string;
 }
 
+interface AvailabilityItem {
+  hour: number;
+  available: boolean;
+}
+
 const CreateAppointment: React.FC = () => {
   const route = useRoute();
   const { user } = useAuth();
@@ -43,6 +48,7 @@ const CreateAppointment: React.FC = () => {
 
   const routeParams = route.params as RouteParams;
 
+  const [availability, setAvailability] = useState<AvailabilityItem[]>([])
   const [showDatePicker, setShowDatePicker] = useState(false)
   const [selectedDate, setSelectedDate] = useState<Date>(new Date())
   const [providers, setProviders] = useState<Provider[]>([]);
@@ -55,6 +61,18 @@ const CreateAppointment: React.FC = () => {
       setProviders(response.data);
     });
   }, []);
+
+  useEffect(() => {
+    api.get(`/providers/${selectedProvider}/day-availability`, {
+      params: {
+        year: selectedDate.getFullYear(),
+        month: selectedDate.getMonth() + 1,
+        day: selectedDate.getDate()
+      }
+    }).then(response => {
+      setAvailability(response.data)
+    })
+  }, [selectedDate, selectedProvider])
 
   const handleSelectprovider = useCallback((providerId: string) => {
     setSelectedProvider(providerId);
