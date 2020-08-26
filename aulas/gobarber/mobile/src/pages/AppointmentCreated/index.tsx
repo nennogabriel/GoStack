9f7b1d762043c222bbef9128138ae7b1ad5faa76
Pedro Moreno <nennogabriel@gmail.com>
2020-08-26 00:13:13 -0300

    13a15 - Criando Estrutura da Tela

--- aulas/gobarber/mobile/src/pages/AppointmentCreated/index.tsx
+++ aulas/gobarber/mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
 
-import { Container } from './styles';
+import { Container, Title, Description, OkButton, OkButtonText } from './styles';
 
 const AppointmentCreated: React.FC = () => {
+
+  const { reset } = useNavigation()
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{
+        name: "Dashboard",
+      }],
+      index: 0
+    })
+  }, [])
+
   return (
     <Container>
-      <Text>AppointmentCreated</Text>
+      <Icon name="check" size={80} color="#04d363" />
+      <Title>AppointmentCreated</Title>
+      <Description>alguma coisa</Description>
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
     </Container>
   );
 };
